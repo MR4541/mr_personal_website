837e12de2164e0720a25265f59e8d8d9b646dc04
Joy523821 <116350136+MR4541@users.noreply.github.com>
2025-06-26 22:33:52 +0800

    menu fix
    fix the problem that the style "position: sticky"  of drop-down menu will not work at all the webpages using header_footer.js

--- scripts/header_footer.js
+++ scripts/header_footer.js
@@ -11,12 +11,25 @@ document.addEventListener("DOMContentLoaded", async function(){
 
         const link2 = document.createElement("link");
         link2.rel = "stylesheet";
-        link2.href = "../style/footer.css"; // 確保路徑正確
-        document.head.appendChild(link2);
+        link2.href = "../style/header.css"; // 確保路徑正確
+        document.head.appendChild(link);
         
-        const response2 = await fetch("../html/footer.html"); //讀檔
+        const response2 = await fetch("../html/menu.html"); //讀檔
         const html2 = await response2.text(); //轉換為純文字
-        document.getElementById("footer-container").innerHTML = html2;
+        const menu_container = document.getElementById("menu-container");
+        menu_container.innerHTML = html2;
+        menu_container.style.position = "sticky";
+        menu_container.style.top = 0;
+        menu_container.style.zIndex = 999;
+
+        const link3 = document.createElement("link");
+        link3.rel = "stylesheet";
+        link3.href = "../style/footer.css"; // 確保路徑正確
+        document.head.appendChild(link3);
+        
+        const response3 = await fetch("../html/footer.html"); //讀檔
+        const html3 = await response3.text(); //轉換為純文字
+        document.getElementById("footer-container").innerHTML = html3;
     }catch (error){
         console.error("載入時發生錯誤：", error);
     }
